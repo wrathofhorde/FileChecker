7fa3dd954372493187ec4da1fa9f8fa4ecb89dfc
wrath <wrathofhorde@gmail.com>
2022-01-03 11:53:12 +0900

    first

---
+++ index.js
@@ -0,0 +1,57 @@
+(function onLoad(params) {
+  const FILE_SIZE = 42236928;
+  const STR_OK = 'OK';
+  const STR_NG = 'NG';
+  const STR_CHECKING = 'Checking...';
+  const HASH_KEY = '7757c2a407df65edbd96f91f719b2c05';
+  const display = document.querySelector(".display");
+  const btnOpen = document.querySelector(".button");
+  const select = document.querySelector(".select");
+
+  btnOpen.addEventListener("click", () => {
+    const event = new MouseEvent("click", {
+      bubbles: true,
+      cancelable: true,
+      view: window,
+    });
+    select.value = "";
+    select.dispatchEvent(event);
+  });
+
+  select.addEventListener("input", (event) => {
+    const file = event.target.files[0];
+
+    if (file.length === 0) {
+      alert("Invalid path");
+      return;
+    }
+    
+    display.innerText = STR_CHECKING;
+    display.classList.remove(STR_NG);
+    display.classList.remove(STR_OK);
+
+    const reader = new FileReader();
+
+    reader.addEventListener("load", (event) => {
+      const binary = event.target.result;
+      const md5 = CryptoJS.MD5(binary).toString();
+      if (HASH_KEY === md5) {
+        display.innerText = STR_OK;
+        display.classList.add(STR_OK);
+      } else {
+        display.innerText = STR_NG;
+        display.classList.add(STR_NG);
+      }
+    });
+
+    file
+      .arrayBuffer()
+      .then((data) => {
+        const blob = new Blob([new Uint8Array(data, 0, data.length)]);
+        reader.readAsBinaryString(blob);
+      })
+      .catch((e) => {
+        alert(e);
+      });
+  });
+})();
